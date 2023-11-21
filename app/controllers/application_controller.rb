f5dd06e5f48480b4f33a1983bd97456ecaaaa10b
kuma <kuma@sonicgarden.jp>
2023-11-22 00:32:55 +0900

    feat: headerのトグルでプロジェクトを選択できるように

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :set_projects
+
+  private
+  def set_projects
+    @projects = Project.default_order
+  end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,6 @@
 class Project < ApplicationRecord
   validates :name, presence: true, uniqueness: true
+
+  # TODO acts_as_listを導入したらpositionが昇順で一番上のプロジェクトをデフォルトで表示するように
+  scope :default_order, -> { order(id: :asc) }
 end
