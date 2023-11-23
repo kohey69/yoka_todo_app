da5cafd871373ddc4bad8796b92a4dfb358d61aa
kuma <kuma@sonicgarden.jp>
2023-11-23 15:27:44 +0900

    fix: taskの設計

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,10 @@
 class Project < ApplicationRecord
+  acts_as_list
+
+  has_many :tasks, -> { order(position: :asc) }
+
   validates :name, presence: true, uniqueness: true
 
   # TODO acts_as_listを導入したらpositionが昇順で一番上のプロジェクトをデフォルトで表示するように
-  scope :default_order, -> { order(id: :asc) }
+  scope :default_order, -> { order(position: :asc) }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_10_100309) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_23_062423) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_10_100309) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "position"
   end
 
+  create_table "tasks", force: :cascade do |t|
+    t.bigint "project_id", null: false
+    t.string "name"
+    t.integer "position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_tasks_on_project_id"
+  end
+
+  add_foreign_key "tasks", "projects"
 end
