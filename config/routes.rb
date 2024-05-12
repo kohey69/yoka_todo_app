50379e7c0251b6ad2d132fc4beec22d523c4028d
kuma <kuma@sonicgarden.jp>
2024-05-12 11:57:54 +0900

    refactor: シングルクォートに

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :projects, only: %i[show new create edit update destroy] do
     resources :tasks, only: %i[create edit update], module: :projects
   end
-  root "home#home"
+  root 'home#home'
 end
