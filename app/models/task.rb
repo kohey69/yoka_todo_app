845fb423d326dc24d4b789e715b093393bd64b4f
kuma <kuma@sonicgarden.jp>
2024-05-19 00:40:50 +0900

    feat: チェックボックスでタスクを完了ステータスにできるように

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,12 +1,27 @@
 class Task < ApplicationRecord
-  extend Enumerize
-
   acts_as_list scope: :project
-  enumerize :status, in: [:in_progress, :completed], default: :in_progress
 
   belongs_to :project
 
+  attribute :completed, :boolean
+  alias_attribute :completed?, :completed
+
   scope :default_order, -> { order(position: :asc) }
-  scope :in_progress, -> { where(status: 'in_progress') }
-  scope :completed, -> { where(status: 'completed') }
+  scope :in_progress, -> { where(completed_at: nil) }
+  scope :completed, -> { where.not(completed_at: nil) }
+
+  def completed
+    self.completed_at.present?
+  end
+
+  def completed=(value)
+    return if value.nil?
+
+    bool_value = ActiveRecord::Type::Boolean.new.cast(value)
+    if bool_value == true && completed_at.blank?
+      self.completed_at = Time.current
+    elsif bool_value == false && completed.present?
+      self.completed_at = nil
+    end
+  end
 end
