aeb513e5d146998aba716cbb4cb729daf42b07c0
kuma <kuma@sonicgarden.jp>
2024-05-19 16:23:46 +0900

    feat: fontawesome導入

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # fontawesomeのアイコンを生成するメソッド
+  def icon(icon_style, icon_name)
+    tag.i(class: ["fa-#{icon_style}", "fa-#{icon_name}"])
+  end
+
+  # fontawesomeの「アイコン＋文字列」を生成するメソッド
+  def icon_with_text(icon_style, icon_name, text)
+    tag.span(icon(icon_style, icon_name), class: 'mx-1') + tag.span(text)
+  end
 end
